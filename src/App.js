800ee147da15d1a675983f8b9960b137a87594e7
fatmanur <fatmanurafyoncu@gmail.com>
2021-06-05 18:42:14 +0300

    statusbar move to mainNavigation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import MainNavigation from './components/Main/MainNavigation';
 import store from './redux/store';
 import {Provider} from 'react-redux';
 import Footer from './components/Footer/Footer';
+import { StatusBar } from 'react-native';
+import {Colors} from './constants/Colors';
 
 export default App = () => {
   return (
